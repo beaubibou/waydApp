f05911234d7b0ff0b3e7c77835f8c29d1b1477ea
bibou <pmestivier@club.fr>
2017-09-02 08:22:24 +0200

    defautCommint

---
+++ app/build/generated/source/buildConfig/debug/com/application/wayd/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.application.wayd;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.application.wayd";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1855;
+  public static final String VERSION_NAME = "Wayd 1.1.";
+}
