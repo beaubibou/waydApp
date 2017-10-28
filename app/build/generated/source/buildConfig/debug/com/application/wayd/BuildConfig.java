a2a28222ce65e0a42700db101847a45ba858dfcf
bibou <pmestivier@club.fr>
2017-10-28 07:35:10 +0200

    ok  recherche maitenant

--- app/build/generated/source/buildConfig/debug/com/application/wayd/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/application/wayd/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.application.wayd";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2569;
+  public static final int VERSION_CODE = 2571;
   public static final String VERSION_NAME = "Wayd.1.1.";
 }
